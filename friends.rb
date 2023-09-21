3896a85fde981002dc4b48d26ea4d21c90cd06f5
Dylan Perry <dylanbperry@gmail.com>
2023-09-21 14:13:03 -0700

    Add a new array

--- friends.rb
+++ friends.rb
@@ -3,3 +3,9 @@ arrays = ["Jeff", "Matt", "Pat", "Gabby", "Josh"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places_to_visit = ["Seattle", "Berlin", "London", "New York City", "Bamf"]
+
+places_to_visit.push("Savannah")
+puts places_to_visit
+
